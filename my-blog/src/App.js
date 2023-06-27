de294dfba99eda95aa1b42810e0f2f58a44ebb45
Nikhitha Sudati <nikhithasudati78@gmail.com>
2023-06-27 10:31:07 -0400

    First Commit
    Initial Commit

---
+++ my-blog/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hello </h1>
+      <div>Welcome to my blog</div>
+    </div>
+  );
+}
+
+export default App;
